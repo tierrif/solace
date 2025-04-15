8408f824cf2c483a2051b2870988b1cdf7a02111
Tierri Ferreira <tierriff@gmail.com>
2025-04-15 23:58:43 +0100

    Add spectra library

---
+++ projects/spectra/src/lib/spectra.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-spectra',
+  imports: [],
+  template: `
+    <p>
+      spectra works!
+    </p>
+  `,
+  styles: ``
+})
+export class SpectraComponent {
+
+}
---
+++ projects/spectra/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of spectra
+ */
+
+export * from './lib/spectra.service';
+export * from './lib/spectra.component';
